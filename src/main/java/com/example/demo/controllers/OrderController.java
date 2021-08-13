d187af53912008504f771d8a0edb7a379b72aad4
Fathima <fathimafabi03@gmail.com>
2021-08-13 07:50:09 +0530

    Added logs.

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,12 +27,14 @@ public class OrderController {
 	
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	
+
+	Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.info("Invalid User");
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
@@ -41,8 +45,11 @@ public class OrderController {
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
+
 		if(user == null) {
+			log.info("Invalid User");
 			return ResponseEntity.notFound().build();
+
 		}
 		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
